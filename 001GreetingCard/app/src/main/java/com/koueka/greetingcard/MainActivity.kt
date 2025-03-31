58e13056502596e718f672108889b07b17a64bcd
Karly KOUEKA <karlykoueka@hotmail.com>
2025-03-31 00:16:51 -0400

    Change the background color
    Followed the instruction and surround the
    text with a Widget via Alt+Enter after
    clicking on the Text composable
    slected Box container then replaced Box
    with Surface, removed unused Box import
    and set the color to Cyan.

--- 001GreetingCard/app/src/main/java/com/koueka/greetingcard/MainActivity.kt
+++ 001GreetingCard/app/src/main/java/com/koueka/greetingcard/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.koueka.greetingcard.ui.theme.GreetingCardTheme
 
@@ -32,10 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi, my name is $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
