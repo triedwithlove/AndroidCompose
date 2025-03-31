2d566789ed79faea9a0ddeaa8ab2c50c2046f62b
Karly KOUEKA <karlykoueka@hotmail.com>
2025-03-30 22:11:15 -0400

    Update the text
    first the text of the Greeting Composable
    has been updated.
    Then inside the GreetingPreview the text
    has been updated to our first name

--- 001GreetingCard/app/src/main/java/com/koueka/greetingcard/MainActivity.kt
+++ 001GreetingCard/app/src/main/java/com/koueka/greetingcard/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hi, my name is $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
-        Greeting("Android")
+        Greeting("Karly")
     }
 }
