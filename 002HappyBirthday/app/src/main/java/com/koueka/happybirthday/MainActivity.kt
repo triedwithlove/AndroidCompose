6e4db62bda7368bcb6bb0ca73b2582dc8a7c238f
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-01 08:23:50 -0400

    Arrange Elements
    This code labs works with Scaffold instead
    of Surface.
    the Column Composable needs to set the
    fillMaxSize for this to work.
    First text font and lineheight have been
    updated.
    The second has been aligned to the end.
    The sceenshots for this have been provided
    in the image folder

--- 002HappyBirthday/app/src/main/java/com/koueka/happybirthday/MainActivity.kt
+++ 002HappyBirthday/app/src/main/java/com/koueka/happybirthday/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.koueka.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -21,10 +26,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                /*    Greeting(
-                        name = "Android",
+                    GreetingText(
+                        message = "Happy Birthday Danielle!",
+                        from = "From Karly",
                         modifier = Modifier.padding(innerPadding)
-                    )*/
+                    )
                 }
             }
         }
@@ -34,27 +40,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-//        modifier = modifier
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
-
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize().padding(8.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 90.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+            //        modifier = modifier
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 
 @Preview(showBackground = true,
-//    showSystemUi = true,
+    showSystemUi = true,
 //    name = "My Preview"
 )
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From Karly")
+        GreetingText(message = "Happy Birthday Danielle!", from = "From Karly")
     }
 }
