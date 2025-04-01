1c0adc599a1287d8db2f02853ae010a2df1201f9
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-01 18:09:12 -0400

    Added an Image
    The provided Image was very heavy 5~ Mega.
    Needed to change this to something less
    heavy. It has been converted to jpg and
    then added to the resource.
    The hardcoded string have also been
    removed to handle later translation in the
    string resource file

--- 002HappyBirthday/app/src/main/java/com/koueka/happybirthday/MainActivity.kt
+++ 002HappyBirthday/app/src/main/java/com/koueka/happybirthday/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,9 +33,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingText(
-                        message = "Happy Birthday Danielle!",
-                        from = "From Karly",
+//                    GreetingText(
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -39,10 +47,48 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
+fun GreetingImage(message: String, from:String, modifier: Modifier = Modifier) {
+    val img = painterResource(id = R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = img,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true,
+    showSystemUi = true,
+//    name = "My Preview"
+)
+@Composable
+fun BirthdayCardImagePreview() {
+    HappyBirthdayTheme {
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
+    }
+}
+
+
+@Composable
 fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize().padding(8.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
     ) {
         Text(
             text = message,
@@ -55,6 +101,7 @@ fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
             text = from,
             fontSize = 36.sp,
             modifier = Modifier
+//                .background(color = Color.Green)
                 .padding(16.dp)
                 .align(alignment = Alignment.End)
         )
@@ -69,6 +116,8 @@ fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Danielle!", from = "From Karly")
+        GreetingText(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text))
     }
 }
