3decddff597b51513bc3e51c58538f286632afca
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-02 03:02:58 -0400

    Compose quadrant
    Build one first and then build the 4.

--- 003ComposePractice/app/src/main/java/com/koueka/composepractice/MainActivity.kt
+++ 003ComposePractice/app/src/main/java/com/koueka/composepractice/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,6 +26,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.koueka.composepractice.ui.theme.ComposePracticeTheme
+import com.koueka.composepractice.ui.theme.bottomEndColor
+import com.koueka.composepractice.ui.theme.bottomStartColor
+import com.koueka.composepractice.ui.theme.topEndColor
+import com.koueka.composepractice.ui.theme.topStartColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,20 +38,166 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                Surface(modifier = Modifier.fillMaxSize()) {
 /*                    BasicCompose(
                         title = stringResource(R.string.basic_title),
                         description = stringResource(R.string.basic_description),
                         message = stringResource(R.string.basic_message),
                         modifier = Modifier.padding(innerPadding)
                     )*/
-                    TaskManager(
+/*                    TaskManager(
                         title = stringResource(R.string.task_manager_title),
                         encouragement = stringResource(R.string.task_manager_encouragement),
                         modifier = Modifier.padding(innerPadding)
+                    )*/
+                    ComposeQuadrant(
+                        title_top_start = stringResource(R.string.quadrant_title_top_start),
+                        description_top_start = stringResource(R.string.quadrant_title_top_start_description),
+                        color_top_start = topStartColor,
+
+                        title_top_end = stringResource(R.string.quadrant_title_top_end),
+                        description_top_end = stringResource(R.string.quadrant_title_top_end_description),
+                        color_top_end = topEndColor,
+
+                        title_bottom_start = stringResource(R.string.quadrant_title_bottom_start),
+                        description_bottom_start = stringResource(R.string.quadrant_title_bottom_start_description),
+                        color_bottom_start = bottomStartColor,
+
+                        title_bottom_end = stringResource(R.string.quadrant_title_bottom_end),
+                        description_bottom_end = stringResource(R.string.quadrant_title_bottom_end_description),
+                        color_bottom_end = bottomEndColor,
+
+                        modifier = Modifier.padding(innerPadding)
+//                        modifier = Modifier
                     )
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun ComposeQuadrant(
+    title_top_start: String, description_top_start: String, color_top_start: Color,
+    title_top_end: String, description_top_end: String, color_top_end: Color,
+    title_bottom_start: String, description_bottom_start: String, color_bottom_start: Color,
+    title_bottom_end: String, description_bottom_end: String, color_bottom_end: Color,
+    modifier: Modifier = Modifier) {
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.weight(1F)) {
+            PartialComposeQuadrant(
+                title = title_top_start,
+                description = description_top_start,
+                color = color_top_start,
+                modifier = Modifier.weight(1F)
+            )
+            PartialComposeQuadrant(
+                title = title_top_end,
+                description = description_top_end,
+                color = color_top_end,
+                modifier = Modifier.weight(1F)
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1f)) {
+            PartialComposeQuadrant(
+                title = title_bottom_start,
+                description = description_bottom_start,
+                color = color_bottom_start,
+                modifier = Modifier.weight(1f)
+            )
+            PartialComposeQuadrant(
+                title = title_bottom_end,
+                description = description_bottom_end,
+                color = color_bottom_end,
+                modifier = Modifier.weight(1F)
+            )
+        }
+    }
+
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Compose Quadrant"
+)
+@Composable
+fun ComposeQuadrantPreview() {
+    ComposePracticeTheme {
+        ComposeQuadrant(
+            title_top_start = stringResource(R.string.quadrant_title_top_start),
+            description_top_start = stringResource(R.string.quadrant_title_top_start_description),
+            color_top_start = topStartColor,
+
+            title_top_end = stringResource(R.string.quadrant_title_top_end),
+            description_top_end = stringResource(R.string.quadrant_title_top_end_description),
+            color_top_end = topEndColor,
+
+            title_bottom_start = stringResource(R.string.quadrant_title_bottom_start),
+            description_bottom_start = stringResource(R.string.quadrant_title_bottom_start_description),
+            color_bottom_start = bottomStartColor,
+
+            title_bottom_end = stringResource(R.string.quadrant_title_bottom_end),
+            description_bottom_end = stringResource(R.string.quadrant_title_bottom_end_description),
+            color_bottom_end = bottomEndColor,
+        )
+    }
+}
+
+
+@Composable
+fun PartialComposeQuadrant(
+    title: String,
+    description: String,
+    color: Color,
+    modifier: Modifier = Modifier) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.background(Color.LightGray)) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = description,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Partial Compose Quadrant"
+)
+@Composable
+fun PartialComposeQuadrantPreview() {
+    ComposePracticeTheme {
+        PartialComposeQuadrant(
+            title = stringResource(R.string.quadrant_title_top_start),
+            description = stringResource(R.string.quadrant_title_top_start_description),
+            topStartColor
+        )
     }
 }
 
@@ -116,7 +270,7 @@ fun BasicCompose(title: String, description: String, message: String, modifier: 
 @Preview(
     showBackground = true,
     showSystemUi = true,
-    name = "Basic Compose"
+    name = "Compose Article"
 )
 @Composable
 fun BasicComposePreview() {
--- 003ComposePractice/app/src/main/java/com/koueka/composepractice/ui/theme/Color.kt
+++ 003ComposePractice/app/src/main/java/com/koueka/composepractice/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//new colors
+val topStartColor = Color(0xFFEADDFF)
+val topEndColor = Color(0xFFD0BCFF)
+val bottomStartColor = Color(0xFFB69DF8)
+val bottomEndColor = Color(0xFFF6EDFF)
