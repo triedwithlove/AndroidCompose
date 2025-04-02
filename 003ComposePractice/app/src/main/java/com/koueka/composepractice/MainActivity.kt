7533b0d1b0f2bef6720fc6547b229a17df82f288
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-01 21:29:18 -0400

    Compose article
    The result is correct on the preview
    but not correct on the phone, so will
    check that later.

--- 003ComposePractice/app/src/main/java/com/koueka/composepractice/MainActivity.kt
+++ 003ComposePractice/app/src/main/java/com/koueka/composepractice/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.koueka.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    BasicCompose(
+                        title = stringResource(R.string.basic_title),
+                        description = stringResource(R.string.basic_description),
+                        message = stringResource(R.string.basic_message),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BasicCompose(title: String, description: String, message: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Top) {
+        val image = painterResource(id = R.drawable.bg_compose_background)
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = message,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Basic Compose"
+)
 @Composable
-fun GreetingPreview() {
+fun BasicComposePreview() {
     ComposePracticeTheme {
-        Greeting("Android")
+        BasicCompose(
+            title = stringResource(R.string.basic_title),
+            description = stringResource(R.string.basic_description),
+            message = stringResource(R.string.basic_message))
     }
 }
