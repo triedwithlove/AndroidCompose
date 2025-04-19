500583e21c8e458ba49d4d8609b95affba22994d
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-19 06:58:35 -0400

    Superheroes Update
    Add Style
    - Cabin font from MaterialTheme
    - Theme from Material Theme
    - Color from Material Theme
    - Shape

---
+++ 017Superheroes/app/src/main/java/com/koueka/superheroes/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.koueka.superheroes.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    //medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp),
+    large = RoundedCornerShape(16.dp)
+)
