4119cf67766a41071dafe1fd31264bfc0416a9e0
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-23 12:47:27 -0400

    Dessert Clicker App
    Project setup

---
+++ 020DessertClicker/app/src/main/java/com/koueka/dessertclicker/model/Dessert.kt
@@ -0,0 +1,11 @@
+
+package com.koueka.dessertclicker.model
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(
+    val imageId: Int,
+    val price: Int,
+    val startProductionAmount: Int
+)
