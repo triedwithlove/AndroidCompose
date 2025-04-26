9123e3977596333959ef375deddad04489f75363
Karly KOUEKA <karlykoueka@hotmail.com>
2025-04-26 06:37:50 -0400

    Unscramble App
    Update code coverage with a new unittest
    to handle the skip.

--- 021UnscrambleApp/app/src/test/java/com/koueka/unscrambleapp/ui/test/GameViewModelTest.kt
+++ 021UnscrambleApp/app/src/test/java/com/koueka/unscrambleapp/ui/test/GameViewModelTest.kt
@@ -99,6 +99,27 @@ class GameViewModelTest {
     }
 
 
+    @Test
+    fun gameViewModel_WordSkipped_ScoreUnchangedAndWordCountIncreased() {
+
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambleWord)
+
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        val lastWordCount = currentGameUiState.currentWordCount
+        viewModel.skipWord()
+        currentGameUiState = viewModel.uiState.value
+
+        //Assert that score remains unchanged after word is skipped.
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+        //Assert that word count is increased by 1 after word is skipped
+        assertEquals(lastWordCount + 1, currentGameUiState.currentWordCount)
+    }
+
+
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
